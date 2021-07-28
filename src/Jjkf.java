568c328e5bdf22bf2b5cc99de5aa3e3d86ed9615
RuslanPankratov <ruslan_skaraev@mail.ru>
2021-07-28 13:44:39 +0300

    ber

--- src/Jjkf.java
+++ src/Jjkf.java
@@ -1,5 +1,17 @@
 public class Jjkf {
     public static void main(String[] args) {
-        System.out.println("sfsd");
+        System.out.println("sfsd");//
     }
+}
+
+class Mer{
+    int a;
+     void ner(){
+         System.out.println(a);
+     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
